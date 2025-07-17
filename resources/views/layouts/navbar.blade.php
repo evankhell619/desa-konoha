30a009cabb4277e535d3a817b895184cf65bd6d3
INF-HANIFAN\INF-EEVNXX <hanifan.hartono@infimedia.co.id>
2025-07-17 23:41:55 +0700

    Initial commit of Desa Konoha

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">
+            <img src="{{ asset('build/assets/img/logo2.png') }}" alt="Logo Desa Konoha" style="height: 60px;">
+            Desa Konoha
+        </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#profildesa">Profil Desa</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#petadesa">Peta Desa</a></li>
+                         <li class="nav-item"><a class="nav-link" href="#informasipublik">Informasi Publik</a></li>
+                         <li class="nav-item"><a class="nav-link" href="#berita">Berita</a></li>
+                         <li class="nav-item"><a class="nav-link" href="#bumdes">Bumdes</a></li>
+                        </ul>
+                </div>
+            </div>
+        </nav>
